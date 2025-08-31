24d8b8db2bf10a78823d9b6effc1d320e42cd0bf
titib57 <thieb.marotta@gmail.com>
2025-08-31 19:36:52 +0200

    rr

--- js/core/login.js
+++ js/core/login.js
@@ -72,7 +72,7 @@ loginForm.addEventListener('submit', async (e) => {
         
         // Redirection vers la page de personnage ou une autre page après un court délai
         setTimeout(() => {
-            window.location.href = "world_map.html";
+            window.location.href = "gestion_personnage.html";
         }, 1500);
 
     } catch (error) {
