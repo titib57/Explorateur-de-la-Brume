3e60f9737e0e791ce4914040914e8a298c83042e
titib57 <thieb.marotta@gmail.com>
2025-09-03 14:47:21 +0200

    ss

--- js/core/questsData.js
+++ js/core/questsData.js
@@ -1,18 +1,23 @@
 // Fichier : js/core/questsData.js
 
 export const questsData = {
-    "initial_adventure_quest": {
-        questId: 'initial_adventure_quest',
-        title: 'Les premiers pas',
-        description: "Trouvez un abri sûr.",
-        objective: {
-            type: 'talk_to_npc',
-            target: 'le-sage',
-            required: 1
-        },
-        rewards: {
-            xp: 50,
-            gold: 25
-        }
+"initial_adventure_quest": {
+    "questId": "initial_adventure_quest",
+    "title": "Un nouveau départ",
+    "description": "Bienvenue. La première chose à faire est de trouver un endroit sûr. Choisissez un lieu que vous pourrez considérer comme votre abri personnel, puis validez-le pour commencer votre aventure. Cet abri sera votre base, votre refuge.",
+    "objective": {
+        "type": "validate_current_location",
+        "action": "define_shelter",
+        "required": 1
+    },
+    "rewards": {
+        "xp": 50,
+        "gold": 25,
+        "items": [
+            {
+                "itemId": "outil-de-construction",
+                "quantity": 1
+            }
+        ]
     }
-};
+}
