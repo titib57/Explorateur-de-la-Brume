f27307374c6ab7d9dddaaf31a50ee81d679064c4
titib57 <thieb.marotta@gmail.com>
2025-09-04 11:20:35 +0200

    vdvd

--- js/modules/shelterManager.js
+++ js/modules/shelterManager.js
@@ -4,7 +4,7 @@
  */
 
 // Importez les fonctions nécessaires de Firebase
-import { doc, getDoc, setDoc, runTransaction } from 'firebase/firestore';
+import { doc, getDoc, setDoc, runTransaction } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 import { auth, db } from "../config/firebase-config.js"; // Assurez-vous d'avoir ce fichier de configuration
 
 const COLLECTION_NAME = "users";
